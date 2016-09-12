4fc9203ad9e49b205954f5a601a5846c55c73e13
zhaolianrui <zhaolianrui1987@163.com>
2016-09-12 17:34:25 +0800

    setup polls App Database

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Qestion(models.Model):
+	"""docstring for Qestion"""
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+	"""docstring for Choice"""
+	question = models.ForeignKey(Qestion,on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
